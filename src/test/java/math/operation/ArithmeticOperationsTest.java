7a3c74245f13f1d9cf21bbd00705a004416b3163
ankitbhatter <43775579+ankitbhatter@users.noreply.github.com>
2019-05-14 19:53:40 +0530

    Update ArithmeticOperationsTest.java

--- src/test/java/math/operation/ArithmeticOperationsTest.java
+++ src/test/java/math/operation/ArithmeticOperationsTest.java
@@ -10,19 +10,17 @@ public class ArithmeticOperationsTest {
 	@Test
 	public void testAdd()
 	{
-	
-		ArithmeticOperations operations = new ArithmeticOperations();
-		Integer actual = operations.add(2, 6);
+		ArithmeticOperations opr = new ArithmeticOperations();
+		Integer add = opr.add(2, 6);
 		Integer expected = 8;
-		assertEquals(expected, actual);
+		assertEquals(expected, add);
 		
 	}
 	@Test
-	public void testtts(){
-		ArithmeticOperations ooArithmeticOperations = new ArithmeticOperations();
-		
-		Integer ahha = ooArithmeticOperations.add(3, 9);
-		Integer exp = 18;
-		assertEquals(exp, ahha);
+	public void testMultiply(){
+		ArithmeticOperations opr = new ArithmeticOperations();
+		Integer multiply = opr.multi(3, 9);
+		Integer expected = 27;
+		assertEquals(expected, multiply);
 	}
 }
