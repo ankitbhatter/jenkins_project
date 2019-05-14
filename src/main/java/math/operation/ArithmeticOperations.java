d9df63d47d819f0cb34a46c2a7b778c8807dc01a
ankitbhatter <43775579+ankitbhatter@users.noreply.github.com>
2019-05-14 19:50:07 +0530

    Update ArithmeticOperations.java

--- src/main/java/math/operation/ArithmeticOperations.java
+++ src/main/java/math/operation/ArithmeticOperations.java
@@ -4,10 +4,11 @@ public class ArithmeticOperations {
 
 	public Integer add(Integer a, Integer b)
 	{
-			System.out.println("Welcome Back Ankit !!!");
+		System.out.println("Welcome Back Ankit !!! We are adding");
 		return a+b;
 	}
 	public Integer multi(Integer a, Integer b){
+		System.out.println("Welcome Back Ankit !!! We are multiplying");
 		return a*b;
 	}
 }
